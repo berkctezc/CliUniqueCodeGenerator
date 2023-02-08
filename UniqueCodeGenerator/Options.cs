1a3697e3674778ae03991efb6d97f71963553500
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2023-02-08 13:29:56 +0300

    set default values to options

--- UniqueCodeGenerator/Options.cs
+++ UniqueCodeGenerator/Options.cs
@@ -3,10 +3,10 @@ namespace UniqueCodeGenerator;
 public class Options
 {
     [Option('c', "charset", Required = true, HelpText = "Charset to be used in generation")]
-    public string Charset { get; set; }
+    public string Charset { get; set; } = "";
 
     [Option('l', "length", Required = true, HelpText = "Length of the generated code")]
-    public uint Length { get; set; }
+    public uint Length { get; set; } = 8;
 
     public void Deconstruct(out string charset, out uint length)
         => (charset, length) = (Charset, Length);
