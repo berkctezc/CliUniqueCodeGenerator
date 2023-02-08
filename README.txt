abf9a51d568b3bb8a7cb3591dfc51bdddf6aa25f
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2023-02-08 13:26:33 +0300

    removed unused using, added readme.txt

---
+++ README.txt
@@ -0,0 +1,7 @@
+# Build
+
+dotnet publish --configuration Release
+
+# Run
+
+dotnet UniqueCodeGenerator.dll --charset ACDEFGHKLMNPRTXYZ234579 --length 8
--- UniqueCodeGenerator/GlobalUsings.cs
+++ UniqueCodeGenerator/GlobalUsings.cs
@@ -1,5 +1,4 @@
 global using System.Collections.Concurrent;
 global using System.Diagnostics;
 global using BenchmarkDotNet.Attributes;
-global using BenchmarkDotNet.Running;
 global using CommandLine;
