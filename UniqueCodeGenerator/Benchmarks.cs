550e70cbad572eca9d78b8b8105b0207545e5567
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2023-03-25 16:49:49 +0300

    benchmark gets default options from fallback options, created test for parallel generate

--- UniqueCodeGenerator/Benchmarks.cs
+++ UniqueCodeGenerator/Benchmarks.cs
@@ -3,7 +3,7 @@ namespace UniqueCodeGenerator;
 public class Benchmarks
 {
     [Benchmark]
-    public void Test()
+    public void StandartGenerate()
     {
         // ~0,0006 duplication in 10_000_000 takes around ~55-57 seconds
         // ~0,0006 duplication in 1_000_000 takes ~4 seconds
@@ -14,14 +14,34 @@ public class Benchmarks
 
         const int testCount = 1_000_000;
 
-        const string charset = "ACDEFGHKLMNPRTXYZ234579";
+        var (charset, length, _) = new Options();
 
-        Parallel.For(0, testCount, _ => codes.Add(UniqueCodeGenerator.Generate(charset)));
+        Parallel.For(0, testCount, _ => codes.Add(UniqueCodeGenerator.Generate(charset, length)));
 
         var uniqueCount = codes.Distinct().ToList().Count;
 
         Console.WriteLine("Unique codes {0}", uniqueCount.ToString());
-        Console.WriteLine((uniqueCount == testCount) ? "Success" : "Failure");
+        Console.WriteLine(uniqueCount == testCount ? "Success" : "Failure");
+        Console.WriteLine("took {0} ms", sw.ElapsedMilliseconds.ToString());
+        sw.Stop();
+    }
+    public void ParallelGenerate()
+    {
+        var sw = Stopwatch.StartNew();
+        sw.Start();
+
+        var codes = new ConcurrentBag<string>();
+
+        const int testCount = 1_000_000;
+
+        var (charset, length, _) = new Options();
+
+        Parallel.For(0, testCount, _ => codes.Add(UniqueCodeGenerator.GenerateParallel(charset, length)));
+
+        var uniqueCount = codes.Distinct().ToList().Count;
+
+        Console.WriteLine("Unique codes {0}", uniqueCount.ToString());
+        Console.WriteLine(uniqueCount == testCount ? "Success" : "Failure");
         Console.WriteLine("took {0} ms", sw.ElapsedMilliseconds.ToString());
         sw.Stop();
     }
