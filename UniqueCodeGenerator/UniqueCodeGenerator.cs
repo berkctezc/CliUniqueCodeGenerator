0457e44e88b854a868792ced968d921654c994a0
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2023-03-25 16:46:50 +0300

    added parallel generate version

--- UniqueCodeGenerator/UniqueCodeGenerator.cs
+++ UniqueCodeGenerator/UniqueCodeGenerator.cs
@@ -18,4 +18,22 @@ public static class UniqueCodeGenerator
 
         return new string(code.ToArray());
     }
+
+    public static string GenerateParallel(string charset, uint length = 8)
+    {
+        var distinctCharset = charset.Distinct();
+
+        var code = new ConcurrentDictionary<long, char>();
+
+        Parallel.For(0, length, i =>
+        {
+            var shuffle = distinctCharset.OrderBy(_ => RandomNumberGenerator.GetInt32(int.MaxValue)).ToArray();
+
+            var randomIndex = RandomNumberGenerator.GetInt32(0, shuffle.Length);
+
+            code[i] = shuffle[randomIndex];
+        });
+
+        return new string(code.Values.ToArray());
+    }
 }
